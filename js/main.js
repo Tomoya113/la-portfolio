42a6b66c5c84cb320f8fcf6697e4947a6be505d5
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-02-06 03:45:20 +0900

    進捗

--- js/main.js
+++ js/main.js
@@ -4,24 +4,41 @@ window.onload = () => {
 
   const image = document.getElementById('image');
 
-  document.getElementById('webS').onmouseover = () => {
-    image.className = "";
-    image.classList.add('onhover', 'ruby');
-  }
+  // document.getElementById('webS').onmouseover = () => {
+  //   image.className = "";
+  //   image.classList.add('onhover', 'ruby');
+  // }
 
-  document.getElementById('webS').onmouseout = () => {
-    image.classList.remove('onhover')
-  }
+  // document.getElementById('webS').onmouseout = () => {
+  //   image.classList.remove('onhover')
+  // }
   
-  document.getElementById('iPhone').onmouseover = () => {
-    image.className = "";
-    image.classList.add('onhover', 'swift');
-  }
+  // document.getElementById('iPhone').onmouseover = () => {
+  //   image.className = "";
+  //   image.classList.add('onhover', 'swift');
+  // }
 
-  document.getElementById('iPhone').onmouseout = () => {
-    image.classList.remove('onhover')
-  }
+  // document.getElementById('iPhone').onmouseout = () => {
+  //   image.classList.remove('onhover')
+  // }
+
+  const result = document.getElementsByClassName('wrapper')
+  const elements = Array.from(result)
+  elements.map( element => {
+    element.addEventListener("click", active)
+  })
 }
+
+let active = e => {
+  const result = document.getElementsByClassName('wrapper')
+  const elements = Array.from(result)
+  elements.map( element => {
+    console.log(element)
+    element.classList.remove('active-content')
+  })
+  e.currentTarget.classList.add('active-content')
+}
+
 
 window.addEventListener("mousemove", e => {
   const card = document.getElementById('card');
